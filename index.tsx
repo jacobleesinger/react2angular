a28505f4e3900e0f3e83044e4b31bb8f34458f13
Jacob Singer <jacobleesinger@gmail.com>
2017-10-04 12:11:46 -0700

    fixing build (hopefully)

--- index.tsx
+++ index.tsx
@@ -25,7 +25,7 @@ export function react2angular<Props>(
 
   return {
     bindings: fromPairs(names.map(_ => [_, '<'])),
-    controller: ['$element', class extends NgComponent<Props> {
+    controller: ['$element', class extends NgComponent<Props, undefined> {
       constructor(private $element: IAugmentedJQuery) {
         super()
       }
